1fcb99533c793d9a4dc847e8a4a8ad4400a1a561
siararisa-iac <126797704+siararisa-iac@users.noreply.github.com>
2025-05-24 12:12:48 +0800

    Added EnhancedTouchSupport for scaling purpose

--- Assets/Scripts/ARInteraction.cs
+++ Assets/Scripts/ARInteraction.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class ARInteraction : MonoBehaviour
 {
-    // To handle rotation for the quiz,
-    // Store the initial value
-    // Update the rotation everytime the user drags left/right
-    // You can determine the direction of the movement by comparing it to the initial position
-    // Make sure to add a rotationSpeed so it will rotate at just the right spped
-
+    [SerializeField] private float minScale = 0.01f;
+    [SerializeField] private float maxScale = 0.2f;
+    [SerializeField] private float rotationSpeed = 0.1f;
+    [SerializeField] private float scaleSpeed = 0.1f;
     private IEnumerator Start(){
         while(InputManager.Instance == null){
             yield return null;
@@ -17,21 +15,33 @@ public class ARInteraction : MonoBehaviour
         InputManager.Instance.OnTouchStarted += HandleTouchStarted;
         InputManager.Instance.OnTouchMoved += HandleTouchMoved;
         InputManager.Instance.OnTouchEnded += HandleTouchEnded;
+        InputManager.Instance.OnPinchUpdated += HandlePinchUpdated;
     }
 
-    private void HandleTouchStarted(Vector2 startPosition){
-        // Get the initial position
+    private void HandleTouchStarted(Vector2 startPosition, Vector2 delta){
+       
     }
 
-    private void HandleTouchMoved(Vector2 updatedPosition){
-        // Do something with the updated position
-        float zPos = transform.position.z;
-        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(updatedPosition.x, updatedPosition.y, zPos));
-        targetPosition.z = zPos;
-        transform.position = targetPosition;
+    private void HandlePinchUpdated(float delta)
+    {
+        //do the scaling here
+        float scaleValue = delta * scaleSpeed * Time.deltaTime;
+        var currentScale = transform.localScale;
+        // Add the scale value to the current scale
+        Vector3 newScale = currentScale + Vector3.one * scaleValue;
+        // Make sure to limit the scale so it doesnt grow too big or too small
+        newScale = Vector3.Max(Vector3.one * minScale, Vector3.Min(Vector3.one * maxScale, newScale));
+        // Set the scale as local
+        transform.localScale = newScale;
     }
 
-    private void HandleTouchEnded(Vector2 endPosition){
+    private void HandleTouchMoved(Vector2 updatedPosition, Vector2 delta){
+        var rotationValue = delta.x * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotationValue, 0);
+       
+    }
+
+    private void HandleTouchEnded(Vector2 endPosition, Vector2 delta){
 
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class InputManager : MonoBehaviour
 {
@@ -11,14 +14,18 @@ public class InputManager : MonoBehaviour
     public static InputManager Instance => instance;
 
 
-    public delegate void TouchEvent(Vector2 position);
+    public delegate void TouchEvent(Vector2 position, Vector2 delta);
     public event TouchEvent OnTouchStarted;
     public event TouchEvent OnTouchEnded;
     public event TouchEvent OnTouchMoved;
 
 
-    private InputControls inputControls;
+    public delegate void PinchEvent(float delta);
+    public event PinchEvent OnPinchUpdated;
 
+    private InputControls inputControls;
+    private Vector2 initialPosition;
+    private float previousPinchDistance = 0;
     private void Awake()
     {
         // Initialize the inputControls
@@ -32,13 +39,17 @@ public class InputManager : MonoBehaviour
         // since this is an ar app, we want to automatically enable ar input
         // so you are free to enable/disable specific action maps
         // based on your design
-          inputControls.ARInput.Enable();
+        inputControls.ARInput.Enable();
         //  inputControls.VehicleMovement.Disable();
+
+        // Enable the EnhancedTouch for it to work
+        EnhancedTouchSupport.Enable();
     }
 
     private void OnDisable()
     {
         inputControls.Disable();  
+        EnhancedTouchSupport.Disable();
     }
 
     private void Start()
@@ -55,23 +66,81 @@ public class InputManager : MonoBehaviour
         // the value (which is set as a Vector 2) every frame, and that is
         // accessible through the 'performed' event
         inputControls.ARInput.TouchPosition.performed += ctx => UpdateTouchPosition(ctx);
+    
+
+        // The below events is similar to the inputControls implementation, except using EnhancedTouch
+        Touch.onFingerDown += OnFingerDown;
+        Touch.onFingerUp += OnFingerUp;
+        Touch.onFingerMove += OnFingerMove;
+    }
+
+    private void OnFingerDown(Finger finger){
+        initialPosition = finger.screenPosition;
+        OnTouchStarted?.Invoke(finger.screenPosition, Vector2.zero);
+    }
+
+    private void OnFingerUp(Finger finger){
+        var delta = finger.screenPosition - initialPosition;
+        OnTouchEnded?.Invoke(finger.screenPosition, delta);
+    }
+
+    private void OnFingerMove(Finger finger){
+        Debug.Log($"Finger with index {finger.index} is at {finger.screenPosition}");
+        var delta = finger.screenPosition - initialPosition;
+        OnTouchMoved?.Invoke(finger.screenPosition, delta);
     }
 
     private void UpdateTouchPosition(InputAction.CallbackContext context){
-        Vector2 contextVal = context.ReadValue<Vector2>();
-        Vector2 getVal = GetTouchPosition();
-        //Debug.Log($"Touch updated: {getVal} vs {contextVal}");
-        OnTouchMoved?.Invoke(GetTouchPosition());
+        Vector2 currentValue = context.ReadValue<Vector2>();
+        // Get the difference between the start position and the current position
+        var delta = currentValue - initialPosition;
+        //OnTouchMoved?.Invoke(currentValue, delta);
     }
 
     private void StartTouch(InputAction.CallbackContext context){
         //Debug.LogWarning($"Touch has started at {GetTouchPosition()}");
-        OnTouchStarted?.Invoke(GetTouchPosition());
+        // Assign the initial position at the start of the touch
+        initialPosition = GetTouchPosition();
+        //OnTouchStarted?.Invoke(initialPosition, Vector2.zero);
     }
 
     private void EndTouch(InputAction.CallbackContext context){
         //Debug.LogWarning($"Touch has ended  at {GetTouchPosition()}");
-        OnTouchEnded?.Invoke(GetTouchPosition());
+        Vector2 currentValue = context.ReadValue<Vector2>();
+         var delta = currentValue - initialPosition;
+        //OnTouchEnded?.Invoke(currentValue, delta);
+    }
+
+
+    private void Update()
+    {
+        if(Touch.activeTouches.Count == 2)
+        {
+            // Store each touch
+            var touch0 = Touch.activeTouches[0];
+            var touch1 = Touch.activeTouches[1];
+
+            // Get actual screen position of each touch
+            Vector2 currentPos0 = touch0.screenPosition;
+            Vector2 currentPos1 = touch1.screenPosition;
+
+            // Get the distance between the two positions
+            float currentDistance = Vector2.Distance(currentPos0, currentPos1);
+
+            // Only run the pinch event when there is a change in the pinch distance
+            if(previousPinchDistance != 0f){
+                float delta = currentDistance - previousPinchDistance;
+                OnPinchUpdated?.Invoke(delta);
+            }
+
+            // Make sure to update this on the next frame
+            previousPinchDistance = currentDistance;
+        }
+        // If more than 2 or less touch count, dont do anything
+        else{
+            // Reset when fewere/more touches are active
+            previousPinchDistance = 0;
+        }   
     }
 
     private Vector2 GetTouchPosition(){
